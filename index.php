cbbaeff0cea514688638981e9ed4ec6696c31503
manke012 <kantemanke010@gmail.com>
2025-02-20 00:13:13 -0900

    Mise à jour appliquee

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ $password = '';
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    header('Location: connect.php');
 } catch (PDOException $e) {
     echo "Erreur de connexion : " . $e->getMessage();
 }
